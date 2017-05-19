4f0168e2753c8584bf6b0e65ff2e34de7bbc5bb2
Jonathan Edwards <edwardjd@colorado.edu>
2017-05-19 14:37:27 -0600

    added Graph DataStructure and methods

---
+++ src/ds/graph/Node.java
@@ -0,0 +1,10 @@
+package ds.graph;
+
+public class Node {
+	public int vertexIdx;
+	public Node next;
+	public Node(int vertexIdx, Node node) {
+		this.vertexIdx = vertexIdx;
+		next = node;
+	}
+}
---
+++ src/ds/graph/Vertex.java
@@ -0,0 +1,10 @@
+package ds.graph;
+
+public class Vertex {
+	String name;
+	Node adjList;
+	Vertex(String name, Node aNode) {
+		this.name = name;
+		this.adjList = aNode;
+	}
+}
